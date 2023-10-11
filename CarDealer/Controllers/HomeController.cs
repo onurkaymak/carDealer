22daa93bb09fc34e6a96509ccd137c4b46bb7fb6
onurkaymak <onurkaymak@outlook.com>
2023-10-11 13:34:24 -0700

    create HomeController, add Index route.

---
+++ CarDealer/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Mvc;
+using CarDealer.Models;
+using System.Lingq;
+
+namespace CarDealer.Controllers
+{
+  public class HomeController : Controller
+  {
+    private readonly CarDealerContext _db;
+
+
+    public HomeController(CarDealerContext db)
+    {
+      _db = db;
+    }
+
+    [HttpGet("/")]
+    public ActionResult Index()
+    {
+      return View();
+    }
+  }
+}

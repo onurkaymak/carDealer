28a68947aab209aff0646b51a810925ebb65c839
onurkaymak <onurkaymak@outlook.com>
2023-10-11 11:57:17 -0700

    create Car class.

---
+++ CarDealer/Models/Car.cs
@@ -0,0 +1,18 @@
+
+namespace CarDealer
+{
+  public class Car
+  {
+    public int CarId { get; set; }
+
+    public string Brand { get; set; }
+
+    public string Model { get; set; }
+
+    public int ModelYear { get; set; }
+
+    public int Mileage { get; set; }
+
+    public string Color { get; set; }
+  }
+}

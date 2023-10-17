bd31b171e46a17e44a08ff192dbe932f260da7eb
onurkaymak <onurkaymak@outlook.com>
2023-10-17 15:16:27 -0700

    add Account Controller.

---
+++ CarDealer/Controllers/AccountController.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using CarDealer.Models;
+using System.Threading.Tasks;
+// using CarDealer.ViewModels
+
+namespace CarDealer.Controllers
+{
+  public class AccountController : Controller
+  {
+    private readonly CarDealerContext _db;
+
+    private readonly UserManager<ApplicationUser> _userManager;
+
+    private readonly SignInManager<ApplicationUser> _signInManager;
+
+    public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, CarDealerContext db)
+    {
+      _userManager = userManager;
+      _signInManager = signInManager;
+      _db = db;
+    }
+  }
+}

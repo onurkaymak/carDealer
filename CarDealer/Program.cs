711d6857cc4e88dc0d21faeb457f4d7e50d6c5fb
onurkaymak <onurkaymak@outlook.com>
2023-10-18 09:37:15 -0700

    set default password settings for Identity.

--- CarDealer/Program.cs
+++ CarDealer/Program.cs
@@ -25,6 +25,17 @@ namespace CarDealer
                 .AddEntityFrameworkStores<CarDealerContext>()
                 .AddDefaultTokenProviders();
 
+      builder.Services.Configure<IdentityOptions>(options =>
+      {
+        // Default Password settings.
+        options.Password.RequireDigit = true;
+        options.Password.RequireLowercase = true;
+        options.Password.RequireNonAlphanumeric = true;
+        options.Password.RequireUppercase = true;
+        options.Password.RequiredLength = 6;
+        options.Password.RequiredUniqueChars = 1;
+      });
+
       WebApplication app = builder.Build();
 
       // app.UseDeveloperExceptionPage();

36e173b06fc89ea1c3411111dba2b4e411f3b667
onurkaymak <onurkaymak@outlook.com>
2023-10-18 10:03:12 -0700

    add Register POST route in Account Controller.

--- CarDealer/Controllers/AccountController.cs
+++ CarDealer/Controllers/AccountController.cs
@@ -2,7 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Identity;
 using CarDealer.Models;
 using System.Threading.Tasks;
-// using CarDealer.ViewModels
+using CarDealer.ViewModels;
 
 namespace CarDealer.Controllers
 {
@@ -29,6 +29,32 @@ namespace CarDealer.Controllers
     public IActionResult Register()
     {
       return View();
+    }
+
+    [HttpPost]
+    public async Task<ActionResult> Register(RegisterViewModel model)
+    {
+      if (!ModelState.IsValid)
+      {
+        return View(model);
+      }
+      else
+      {
+        ApplicationUser user = new ApplicationUser { UserName = model.Email };
+        IdentityResult result = await _userManager.CreateAsync(user, model.Password);
+        if (result.Succeeded)
+        {
+          return RedirectToAction("Index");
+        }
+        else
+        {
+          foreach (IdentityError error in result.Errors)
+          {
+            ModelState.AddModelError("", error.Description);
+          }
+          return View(model);
+        }
+      }
     }
   }
 }
